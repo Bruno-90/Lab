91d32aa9190ce832937538f2a8afe4bb2254cf92
MANUPAIS2200 <manupais22@gmail.com>
2021-08-22 22:55:59 -0300

    Creacion de clases con dominio

---
+++ Dominio/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+  public   class Usuario
+    {
+        public int ID { get; set; }
+        public int IDContacto { get; set; }
+        public int IDDepartamento { get; set; }
+        public int IDPermiso { get; set; }
+    }
+}
